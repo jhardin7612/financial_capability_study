48de6f5d60aaca20e48b938c85743db3d6f60a84
jasmine hardin <jasmine.hardin@spaceforce.mil>
2024-07-19 14:25:12 -0400

    added app file to begin streamlit front end work

---
+++ src/app.py
@@ -0,0 +1,7 @@
+"""
+Generate Hexbin map of United states tht show various features related to financial capability study
+"""
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Exploring Financial Capability Across The United States of America")
